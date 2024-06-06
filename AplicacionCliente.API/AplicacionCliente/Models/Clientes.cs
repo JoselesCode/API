ffefd3453458d1d1c62065176b51b828e812c9dc
Josma <107312332+JoseMantis@users.noreply.github.com>
2024-06-06 11:30:43 -0400

    Update Clientes.cs

--- AplicacionCliente.API/AplicacionCliente/Models/Clientes.cs
+++ AplicacionCliente.API/AplicacionCliente/Models/Clientes.cs
@@ -3,7 +3,7 @@
     public class Clientes
     {
         public int Id { get; set; }
-        public string? Edad { get; set; }
+        public string? RazonSocial { get; set; }
         public string? Nombre { get; set; }    
     }
 }
