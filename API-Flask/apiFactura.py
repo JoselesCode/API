bfc40c0fb4d6adaef9be24e20b99e0c5a48acbe6
unknown <Cetecom@SJAPCA03LC0616.duoc.acad>
2024-06-08 11:24:28 -0400

    Avance en clases

--- API-Flask/apiFactura.py
+++ API-Flask/apiFactura.py
@@ -43,31 +43,31 @@ class Factura(Resource):
             cliente_json = cliente_response.json()
 
         # Solicitud a la API de Productos (para obtener el precio del producto)
-            producto_response = requests.get(f"{url_producto}/{json_data['producto_id']}")
+            producto_response = requests.get(f"{url_producto}/{json['producto_id']}")
             if producto_response.status_code == 200:
                 producto_json = producto_response.json()
 
         # Solicitud a la API de Logística (para verificar el stock)
-                logistica_response = requests.get(f"{url_logistica}/{json_data['producto_id']}")
+                logistica_response = requests.get(f"{url_logistica}/{json['producto_id']}")
                 if logistica_response.status_code == 200:
                     logistica_json = logistica_response.json()
         #Verificar Stock
-                    if logistica_json['stock'] >= json_data['cantidad']:
+                    if logistica_json['stock'] >= json['cantidad']:
         #Actualizar datos de boleta
                         objRespuesta["id_cliente"] = cliente_json["id"]
                         objRespuesta["id_producto"] = json["producto_id"]
-                        objRespuesta["cantidad"] = json["cantidad"]
+                        objRespuesta["cantidad"] = logistica_json["cantidad"]
                         objRespuesta["precio"] = producto_json["precio"]
-                        objRespuesta["total_venta"] = objRespuesta["precio"] * json["cantidad"]
-                        objRespuesta["nombre_cliente"] = json["razonSocial"] 
-                        objRespuesta["direccion_cliente"] = json["direccion"]
+                        objRespuesta["total_venta"] = objRespuesta["precio"] * logistica_json["cantidad"]
+                        objRespuesta["nombre_cliente"] = cliente_json["razonSocial"] 
+                        objRespuesta["direccion_cliente"] = cliente_json["direccion"]
 
                         # Actualizar el stock en la API de Logística Stock
-                        requests.put(f"{url_logistica}/{json_data['producto_id']}",
-                        json={'stock': logistica_json['stock'] - json_data['cantidad']})
+                        requests.put(f"{url_logistica}/{json['producto_id']}",
+                        json={'stock': logistica_json['stock'] - json['cantidad']})
                         return objRespuesta
                     else:
-                        return {"error" : "Stock insuficiente}, 400
+                        return {"error" : "Stock insuficiente"}, 400
                 else:
                     return {"error" : "API de Logistica no responde"}, 500
             else:
@@ -76,7 +76,6 @@ class Factura(Resource):
             return {"error" : "API de Clientes no responde"}, 500
                         
 
-        #return { "id_producto" : 5, "cantidad": 10, "id_cliente": 2}
         return objRespuesta
 
 api.add_resource(Factura, "/api/boleta", {"origins" : "*"})
