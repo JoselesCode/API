60b6552bb72a266cd1b53aa4812b7f5a8187d7c8
Jhustinpage <107010258+Jhustinpage@users.noreply.github.com>
2024-06-09 15:54:10 -0400

    Comentarios  add  a ClientesContext.cs

--- AplicacionCliente.API/AplicacionCliente/Models/ClientesContext.cs
+++ AplicacionCliente.API/AplicacionCliente/Models/ClientesContext.cs
@@ -1,12 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+//Contexto para la gestión de clientes en AplicacionCliente.
+using Microsoft.EntityFrameworkCore;
 
 namespace AplicacionCliente.Models;
 
 public class ClientesContext : DbContext
 {
-    public ClientesContext(DbContextOptions<ClientesContext> options) : base(options) {
+    public ClientesContext(DbContextOptions<ClientesContext> options) : base(options) {    //Constructor de la clase que recibe las opciones de configuración del contexto de la DB.
     }
 
-    public DbSet<Clientes> Clientes {get; set;} = null!;
+    public DbSet<Clientes> Clientes {get; set;} = null!;    //Propiedad que representa una colección de clientes, que se mapea a la tabla de clientes.
 }
 
